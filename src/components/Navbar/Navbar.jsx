0ce6928459d406b3d45040ac04a631901506942b
Mahjabeen244 <247266@students.au.edu.pk>
2026-07-10 11:05:47 +0500

    added links for pages in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,24 @@ function Navbar() {
   return (
     <nav className={styles.Navbar}>
       <div className={styles.logo}>Task Manager</div>
+      <ul className={styles.links}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
+export default Navbar;
