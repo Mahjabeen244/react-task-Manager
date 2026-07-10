ffff4319a7a9efb4f4c7fb7ab878e5a5048f43be
Mahjabeen244 <247266@students.au.edu.pk>
2026-07-10 11:46:43 +0500

    1st working route "about"

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p>This is the Todo List Application build with react</p>
+    </div>
+  );
+}
+
+export default About;
