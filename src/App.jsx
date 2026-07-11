5f215f0a7d384b2c4429d4a2f7d021a8ece4d4c0
Mahjabeen244 <247266@students.au.edu.pk>
2026-07-11 15:51:46 +0500

    updated app.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import About from "./pages/About/About";
@@ -9,19 +10,24 @@ import Register from "./pages/Register/Register";
 
 function App() {
   const location = useLocation();
-  const showNavbar = location.pathname !== "/";
+  const isHome = location.pathname === "/";
 
   return (
-    <div>
-      {showNavbar && <Navbar />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+    <div className="app-layout">
+      {!isHome && <Navbar />}
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+
+      {!isHome && <Footer />}
     </div>
   );
 }
