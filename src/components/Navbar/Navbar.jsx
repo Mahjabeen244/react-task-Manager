f22b234b44d14a1b60b118a25caa661163b006ac
Mahjabeen244 <247266@students.au.edu.pk>
2026-07-11 17:11:02 +0500

    updated navbar for verification (context)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
+import { useVerification } from "../../context/VerificationContext";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
+  const { isVerified, endSession } = useVerification();
+
   return (
     <nav className={styles.navbar}>
       <Link to="/" className={styles.logo}>
@@ -9,12 +12,20 @@ function Navbar() {
       </Link>
 
       <div className={styles.authLinks}>
-        <Link to="/login" className={styles.loginLink}>
-          Login
-        </Link>
-        <Link to="/register" className={styles.registerBtn}>
-          Register
-        </Link>
+        {isVerified ? (
+          <button className={styles.logoutBtn} onClick={endSession}>
+            Logout
+          </button>
+        ) : (
+          <>
+            <Link to="/login" className={styles.loginLink}>
+              Login
+            </Link>
+            <Link to="/register" className={styles.registerBtn}>
+              Register
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
